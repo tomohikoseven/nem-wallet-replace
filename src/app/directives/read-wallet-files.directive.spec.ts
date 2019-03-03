12eed0e775aabe41d5565fb63b947a2797f7ec1d
tomohikoseven <tomohikoseven0905@gmail.com>
2019-03-03 12:26:12 +0900

    read-wallet-files directive add

---
+++ src/app/directives/read-wallet-files.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ReadWalletFilesDirective } from './read-wallet-files.directive';
+
+describe('ReadWalletFilesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ReadWalletFilesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/read-wallet-files.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[read-wallet-files]'
+})
+export class ReadWalletFilesDirective {
+
+  constructor() { }
+
+}
